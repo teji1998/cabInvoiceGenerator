456da8fc8ebcadfb9ea22645d79f35b43fc70893
“teji1998” <tejasvinirpk@gmail.com>
2020-08-31 09:20:54 +0530

    Added:Test case and code to find minimum fare if it is less than total fare

--- src/test/java/cabinvoicegenerator/InvoiceGeneratorTest.java
+++ src/test/java/cabinvoicegenerator/InvoiceGeneratorTest.java
@@ -14,5 +14,14 @@ public class InvoiceGeneratorTest {
 		double fare = invoiceGenerator.calculateFare(distance, time);
 		Assert.assertEquals(25, fare, 0.0);
 	}
+
+	@Test
+	public void givenLessDistanceOrTime_WhenCalculated_ShouldReturnMinimumFare() {
+		InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+		double distance = 0.1;
+		int time = 1;
+		double fare = invoiceGenerator.calculateFare(distance, time);
+		Assert.assertEquals(5, fare, 0.0);
+	}
 }
 
