25459c133fbc23b25f6710ec961868eaf20f3aab
“teji1998” <tejasvinirpk@gmail.com>
2020-09-05 04:14:54 +0530

    Refactored:Test cases to use mockito

--- src/main/java/cabinvoicegenerator/InvoiceSummary.java
+++ src/main/java/cabinvoicegenerator/InvoiceSummary.java
@@ -1,10 +1,5 @@
 package cabinvoicegenerator;
 
-import lombok.EqualsAndHashCode;
-
-//Used for Equal and hash methods
-@EqualsAndHashCode
-
 public class InvoiceSummary {
 	private final int numberOfRides;
 	private final double totalFare;
@@ -14,5 +9,15 @@ public class InvoiceSummary {
 		this.numberOfRides = numberOfRides;
 		this.totalFare = totalFare;
 		this.totalFareAverage = this.totalFare / this.numberOfRides;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		InvoiceSummary summary = (InvoiceSummary) o;
+		return numberOfRides == summary.numberOfRides &&
+				  Double.compare(summary.totalFare, totalFare) == 0 &&
+				  Double.compare(summary.totalFareAverage, totalFareAverage) == 0;
 	}
 }
--- src/test/java/cabinvoicegenerator/InvoiceServiceTest.java
+++ src/test/java/cabinvoicegenerator/InvoiceServiceTest.java
@@ -5,9 +5,14 @@ import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
 
+@RunWith(MockitoJUnitRunner.class)
 public class InvoiceServiceTest {
 
+	@Mock
 	InvoiceService invoiceService;
 	String userId = "bts";
 	Ride[] rides;
